c18f9450820fcd43da48a8e879382825e4e11ec0
abhishekrs4 <abhishek.r.satyanarayana.4@gmail.com>
2023-04-04 22:59:37 +0200

    added function to plot losses of 2 sets in a single plot

--- plot_utils.py
+++ plot_utils.py
@@ -28,3 +28,34 @@ def plot_losses(list_losses, title):
     plt.legend(fontsize=20)
     plt.grid()
     return fig
+
+def plot_losses_2(list_losses_1, list_losses_2, title, label_1, label_2):
+    """
+    ---------
+    Arguments
+    ---------
+    list_losses_1: list or ndarray
+        a list or a numpy array of losses
+    list_losses_2: list or ndarray
+        a list or a numpy array of losses
+    title: str
+        title for the plot
+
+    -------
+    Returns
+    -------
+    fig: matplotlib figure object
+        returns a matplotlib figure object
+    """
+    length_losses = len(list_losses_1)
+    fig = plt.figure(figsize=(12, 8))
+    plt.plot(np.arange(1, length_losses+1), list_losses_1, "o-", label=label_1)
+    plt.plot(np.arange(1, length_losses+1), list_losses_2, "o-", label=label_2)
+    plt.title(title, fontsize=20)
+    plt.xlabel("epoch", fontsize=20)
+    plt.ylabel("loss", fontsize=20)
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.legend(fontsize=20)
+    plt.grid()
+    return fig
